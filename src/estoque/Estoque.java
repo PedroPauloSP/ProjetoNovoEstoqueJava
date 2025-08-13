85f9b18514321ad88493ece291678bf510e88de7
PedroPaulo <pedropsantospereira@gmail.com>
2025-08-12 21:25:00 -0300

    Remoção dos comentários da classe estoque

--- src/estoque/Estoque.java
+++ src/estoque/Estoque.java
@@ -26,44 +26,7 @@ import conexao.Conexao;
 
         
         
-     /*   
-     try {
- 
- Class.forName( "com.mysql.cj.jdbc.Driver" );
- System.out.println( "Driver JDBC carregado" );
- } catch ( ClassNotFoundException cnfe ) {
- System.out.println( "Driver JDBC nao encontrado : " +
- cnfe.getMessage() );
- }
- Connection con = null;
-
- try {
- 
- con = DriverManager.getConnection("jdbc:mysql://localhost:3306/estoque","root","29Pla#174");
-
- System.out.println( "Conexao com o banco de dados estabelecida." );
- } catch ( SQLException sqle ) {
- System.out.println( "Erro na conexao ao Bando de Dados : " +sqle.getMessage() );  
- }
     
- Statement stmt = null;
- 
- try {
- stmt = con.createStatement();
- System.out.println( "Pronto para execucao de comandos sql." );
- } catch ( SQLException sqle ) {
- System.out.println( "Erro no acesso ao Bando de Dados : " +
- sqle.getMessage() );
- }
- 
- 
-
-}catch (SQLException sqle ){
-
-}
-    }
-}       
-        */
         
          
    
